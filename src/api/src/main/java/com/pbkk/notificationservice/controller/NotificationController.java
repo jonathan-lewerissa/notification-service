2e8ca9e17eeb1ad34e50283f034adc1a6d7825ee
Indra <vinsensiusindra@gmail.com>
2019-05-09 18:41:56 +0700

    add endpoints, update config

--- src/api/src/main/java/com/pbkk/notificationservice/controller/NotificationController.java
+++ src/api/src/main/java/com/pbkk/notificationservice/controller/NotificationController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ public class NotificationController {
 	@Autowired
 	NotificationRepository notificationRepository;
 	
+	/*
 	@GetMapping("/notifications")
 	public List<Notification> getAllNotifications() {
 		return notificationRepository.findAll();
@@ -29,6 +31,26 @@ public class NotificationController {
 	public Notification createNotification(@Valid @RequestBody Notification notification) {
 		return notificationRepository.save(notification);
 	}
+	*/
 	
-//	@GetMapping("/notifications/{id}")
+	@GetMapping("/notifications/{id}")
+	public List<Notification> getAllNotifications(@PathVariable Integer id) {
+		return notificationRepository.findAll();
+	}
+	
+	@PostMapping("/notification-service/{orderId}/user/{userId}/serve")
+	public Notification serveNotification(@Valid @RequestBody Notification notification, @PathVariable Integer id, @PathVariable Integer userId) {
+		// Post isinya konten notifikasi. To do CRUD
+		return notificationRepository.save(notification);
+	}
+	
+	@PostMapping("/notification-service/{orderId}/user/{userId}/deliver")
+	public Notification deliverNotification(@Valid @RequestBody Notification notification, @PathVariable Integer userId) {
+		return notificationRepository.save(notification);
+	}
+	
+	@PostMapping("/notification-service/{orderId}/user/{userId}/arrival")
+	public Notification arrivalNotification(@Valid @RequestBody Notification notification, @PathVariable Integer userId) {
+		return notificationRepository.save(notification);
+	}
 }
